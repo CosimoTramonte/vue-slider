5460cb258064603589261810b434959dc115279a
CosimoTramonte <cosimotramonte7@gmail.com>
2023-04-13 14:42:56 +0200

    add arrow sliders

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ createApp({
 
     data(){
         return{
-            counter: 5,
+            counter: 0,
             image: [
                 "img/01.webp",
                 "img/02.webp",
@@ -14,6 +14,17 @@ createApp({
                 "img/04.webp",
                 "img/05.webp"
             ],
+        }  
+    },
+
+    methods:{
+
+        changeImg(next){
+            if(next) this.counter++
+            else this.counter--
+
+            if(this.counter > this.image.length - 1) this.counter = 0;
+            else if(this.counter < 0) this.counter = this.image.length - 1
         }
     }
 
